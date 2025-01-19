f30783787e753a0809c45c6d228b4c08ea01b5e9
Egor Tarasenko <egor@ponder.co>
2025-01-19 16:27:51 +0200

    FEAT: Update all_the_apps

--- all_the_apps.sh
+++ all_the_apps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Open URLs in Google Chrome
-open -a "Google Chrome" "http://localhost:8080" "http://localhost:8888" "http://localhost:8088" "http://localhost:8089" 
+open -a "Google Chrome" "http://localhost:8089" "http://localhost:8888" "http://localhost:8088" "http://localhost:8080"
